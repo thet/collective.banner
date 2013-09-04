21ef09f4c23e370489ec16a9b7224945bb83d8f7
Johannes Raggam <raggam-nl@adm.at>
2013-09-04 14:04:31 +0200

    refactoring pt1

--- collective/carousel/upgrades/__init__.py
+++
@@ -1,8 +0,0 @@
-from Products.CMFCore.utils import getToolByName
-
-
-def run_all_import_steps(context):
-    context = getToolByName(context, "portal_setup")
-    context.runAllImportStepsFromProfile(
-        'profile-collective.carousel:default',
-        purge_old=False)
